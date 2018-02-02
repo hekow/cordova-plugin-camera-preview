b7e4398138d4ed94a593d2fcdb4968f304557cc9
Flavian Jacquot <flavian.jacquot@gmail.com>
2018-02-02 01:48:58 +0100

    correct error handle

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -86,6 +86,7 @@ public class CameraActivity extends Fragment {
   public int height;
   public int x;
   public int y;
+  public int[] pixels;
 
   public void setEventListener(CameraPreviewListener listener){
     eventListener = listener;
@@ -116,6 +117,7 @@ public class CameraActivity extends Fragment {
 
       //set box position and size
       FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(width, height);
+	  pixels = new int[previewSize.width * previewSize.height];  
       layoutParams.setMargins(x, y, 0, 0);
       frameContainerLayout = (FrameLayout) view.findViewById(getResources().getIdentifier("frame_container", "id", appResourcesPackage));
       frameContainerLayout.setLayoutParams(layoutParams);
@@ -445,7 +447,39 @@ public class CameraActivity extends Fragment {
     }
   };
 
+  static public void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {  
+              
+            final int frameSize = width * height;  
   
+            for (int j = 0, yp = 0; j < height; j++) {       int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;  
+              for (int i = 0; i < width; i++, yp++) {  
+                int y = (0xff & ((int) yuv420sp[yp])) - 16;  
+                if (y < 0)  
+                  y = 0;  
+                if ((i & 1) == 0) {  
+                  v = (0xff & yuv420sp[uvp++]) - 128;  
+                  u = (0xff & yuv420sp[uvp++]) - 128;  
+                }  
+  
+                int y1192 = 1192 * y;  
+                int r = (y1192 + 1634 * v);  
+                int g = (y1192 - 833 * v - 400 * u);  
+                int b = (y1192 + 2066 * u);  
+  
+                if (r < 0)                  r = 0;               else if (r > 262143)  
+                   r = 262143;  
+                if (g < 0)                  g = 0;               else if (g > 262143)  
+                   g = 262143;  
+                if (b < 0)                  b = 0;               else if (b > 262143)  
+                   b = 262143;  
+  
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);  
+              }  
+            }  
+          }  
+    } 
+	
+
     PreviewCallback previewPictureCallback = new PreviewCallback(){
     public void onPreviewFrame(byte[] data, Camera arg1){
       Log.d(TAG, "CameraPreview previewPictureCallback");
@@ -467,8 +501,8 @@ public class CameraActivity extends Fragment {
         // // Check if matrix has changed. In that case, apply matrix and override data
         // if (!matrix.isIdentity()) {
 			        Log.d(TAG, "CameraPreview Decoding"+data.length);
-
-          Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+		int[] pixels; 
+decodeYUV420SP(pixels)
 		  int pixel = bitmap.getPixel(bitmap.getWidth()/2,bitmap.getHeight()/2);
           // bitmap = applyMatrix(bitmap, matrix);
 
